c8747cf55327ef03af72b1b05306f4d74df2d26b
vkravc <vkravchik@gmail.com>
2019-11-29 11:05:54 +0200

    add audio play but with bug

--- app.js
+++ app.js
@@ -7,9 +7,37 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}`)
 });
 
+// Test Ping - Pong
 client.on('message', msg => {
   if (msg.content === 'ping') {
     msg.reply('Pong!');
+  }
+});
+
+const ytdl = require('ytdl-core');
+
+client.on('message', msg => {
+  if (!msg.guild) return;
+
+  if (msg.content.includes('!play')) {
+    if (msg.member.voiceChannel) {
+      msg.member.voiceChannel.join().then(connection => {
+        msg.reply(`Hello!`);
+
+        const link = msg.content.split(' ')[1];
+        const dispatcher = connection.playStream(ytdl(link, {filter: 'audioonly'}));
+
+        dispatcher.on('error', e => {
+          console.log(e);
+        });
+
+        dispatcher.setVolume(0.7);
+      }).catch(err => {
+        console.log(err);
+      })
+    } else {
+      msg.reply('Join to voice chanel, please');
+    }
   }
 });
 
